6311f3116dc4bb109f91e6de97ebd9241a294652
aboubacar108 <108593484+aboubacar108@users.noreply.github.com>
2023-01-18 20:03:09 -0500

    Update app.js

--- starter/app.js
+++ starter/app.js
@@ -8,7 +8,7 @@ const connectDB = require('./db/connect')
 const authenticateUser = require('./middleware/authentication')
 // routers
 const authRouter = require('./routes/auth')
-const auditsRouter = require('./routes/audits')
+const jobsRouter = require('./routes/jobs')
 
 
 // error handler
@@ -20,7 +20,7 @@ app.use(express.json());
 
 // routes
 app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/audits',authenticateUser, auditsRouter)
+app.use('/api/v1/jobs',authenticateUser, jobsRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
